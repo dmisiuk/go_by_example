83fa2ca9353ac83f29db29a338fb36aca8f180bf
Dzmitry Misiuk <minsler.by@gmail.com>
2016-12-27 15:25:20 +0300

    added variables

---
+++ variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a string  = "first"
+	fmt.Println(a)
+
+	var b,c int = 1,2
+	fmt.Println(b,c)
+
+	var d bool
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
