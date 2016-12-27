3cb30e483da10ff63142c0dbc2e3ee4530fc5fe0
Dzmitry Misiuk <minsler.by@gmail.com>
2016-12-27 05:07:51 +0300

    init. Added values and hello_world

---
+++ values.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("a+b=", "a"+"b")
+	fmt.Println("1+2=", 1+2)
+	fmt.Println("1.0/0.3=", 1.0/0.3)
+	fmt.Println("true && false", true && false)
+}
