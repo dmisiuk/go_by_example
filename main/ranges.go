0200e03b43b7974a6ec4e1dc8219207459f9e380
Dzmitry Misiuk <minsler.by@gmail.com>
2016-12-29 17:03:08 +0300

    added ranges

---
+++ main/ranges.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := []int{1, 2, 3}
+
+	fmt.Println(a)
+
+	sum := 0
+
+	for _, num := range a {
+		sum += num
+	}
+
+	fmt.Println("sum:", sum)
+
+	m := map[string]string{"1": "first", "2":"second"}
+
+	for k,v := range m {
+		fmt.Println(k, "->", v)
+	}
+
+	for i,c := range "умный" {
+		fmt.Println(i ,"->", c)
+	}
+
+}
