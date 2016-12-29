bc6190474116c2d3d71fe4583ee83e02ef3062db
Dzmitry Misiuk <minsler.by@gmail.com>
2016-12-29 17:28:18 +0300

    added variadic functions

---
+++ main/variadic_functions.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("sum(1,2)=", sum(1, 2))
+	fmt.Println("sum(1,2,3)=", sum(1, 2, 3))
+
+	s := []int{10, 20, 40}
+	fmt.Println("sum of ", s, "=", sum(s...))
+
+}
+
+func sum(nums ... int) int {
+	sum := 0
+	for _, n := range nums {
+		sum += n
+	}
+	return sum
+}
