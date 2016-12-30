0f0a6394b5bcb19cd1173cd08223a27fa2cad84b
Dzmitry Misiuk <minsler.by@gmail.com>
2016-12-31 00:59:59 +0300

    added synchronization for goroutines

---
+++ main/syncronization_goroutines.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Println("working...")
+	time.Sleep(time.Second)
+	fmt.Println("Done")
+
+	done <- true
+}
+
+func main() {
+	done := make(chan bool)
+
+	go worker(done)
+
+	<-done
+}
