7d54c0bc43d4153410db30dfd5f8d3ded045fc85
Dzmitry Misiuk <minsler.by@gmail.com>
2016-12-30 22:40:29 +0300

    added pointers

---
+++ main/pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func zeroval(v int) {
+	v = 3
+}
+
+func zeropt(v *int)  {
+	*v = 1
+}
+
+func main() {
+	d := 10
+	fmt.Println(d)
+
+	zeroval(d)
+	fmt.Println(d)
+
+	zeropt(&d)
+	fmt.Println(d)
+}
