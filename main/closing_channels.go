486012c1f42ce4080c2c957938002844df69a2f2
Dzmitry Misiuk <minsler.by@gmail.com>
2016-12-31 02:00:58 +0300

    added closing channels

---
+++ main/closing_channels.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	jobs := make(chan int, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			j, more := <-jobs
+			if more {
+				fmt.Println("received jobs", j)
+			} else {
+				fmt.Println("received all jobs")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 3; i++ {
+		fmt.Println("send jobs", i)
+		jobs <- i
+	}
+	close(jobs)
+	fmt.Println("sent all jobs")
+	<-done
+}
