89e42d4d064c11335251cb5eb87398aaba222340
Dzmitry Misiuk <minsler.by@gmail.com>
2016-12-31 15:31:47 +0300

    added atomic counter

--- main/atomic_counter.go
+++ main/atomic_counter.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
 func main() {
+	var counter uint64 = 0
+
+	for i := 0; i < 50; i++ {
+		go func() {
+			for {
+				atomic.AddUint64(&counter, 1)
+				time.Sleep(time.Nanosecond)
+			}
+		}()
+	}
+	time.Sleep(time.Second)
+
+	value := atomic.LoadUint64(&counter)
+	fmt.Println(value)
 }
